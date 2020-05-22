37d8d36c3a3c07ace489d8bb141e26eb667c712a
thunh <thunh@tinhvan.com>
2020-05-22 12:02:27 +0700

    Update favicon

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function Home() {
+    return (
+        <Button variant="contained" color="primary">
+            Hello Word
+        </Button>
+    );
+}
+
+export default Home;
